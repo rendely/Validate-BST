a62aa7499015305175fe197050215663aba78a2b
matthew rendely <matthew.rendely@gmail.com>
2023-12-07 21:39:54 -0800

    done

--- javascript/validBTS.js
+++ javascript/validBTS.js
@@ -7,7 +7,25 @@ class Node {
 }
 
 function isValidBST(root) {
-  // type your code here
+  
+  // check if this node is balanced, if not, return false
+  if (!!root.left && root.left.val > root.val) return false
+  if (!!root.right && root.right.val < root.val) return false
+  
+  // if left node exists, check if it's balanced
+  if (root.left) {
+   const checkLeft = isValidBST(root.left);
+   if (!checkLeft) return false
+  }
+
+  // if right node exists, check if it's balanced
+  if (root.right) {
+    const checkRight = isValidBST(root.right);
+    if (!checkRight) return false
+  }
+
+  // return true if all checks passed to this point
+  return true
 }
 
 if (require.main === module) {
@@ -17,6 +35,9 @@ const T = (v, l, r) => new Node(v, l, r)
 
 console.log("Expecting: true");
 console.log("=>", isValidBST(T(2, T(1), T(3))));
+
+console.log("Expecting: false");
+console.log("=>", isValidBST(T(2, T(4), T(0))));
 
 }
 
